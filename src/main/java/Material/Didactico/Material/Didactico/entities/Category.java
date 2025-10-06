b80895230ee2c9e66aa328a63f0495558e043a03
AJAX411 <buitragoramires@gmail.com>
2025-10-06 09:56:51 -0500

    contenido en package entities

--- src/main/java/Material/Didactico/Material/Didactico/entities/Category.java
+++ src/main/java/Material/Didactico/Material/Didactico/entities/Category.java
@@ -1,4 +1,45 @@
 package Material.Didactico.Material.Didactico.entities;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 public class Category {
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(getId(), category.getId()) && Objects.equals(getName(), category.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
 }
--- src/main/java/Material/Didactico/Material/Didactico/entities/Material.java
+++ src/main/java/Material/Didactico/Material/Didactico/entities/Material.java
@@ -1,5 +1,66 @@
 package Material.Didactico.Material.Didactico.entities;
 
-public class Material {
+import java.io.Serializable;
+import java.util.Objects;
 
+
+public class Material {
+    private Long id;
+    private String nombre;
+    private double precio;
+    private String tipo;
+
+    public Material() {
+    }
+
+    public Material(Long id, String nombre, double precio, String tipo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.tipo = tipo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Material material)) return false;
+        return Double.compare(material.getPrecio(), getPrecio()) == 0 && Objects.equals(getId(), material.getId()) && Objects.equals(getNombre(), material.getNombre()) && Objects.equals(getTipo(), material.getTipo());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getNombre(), getPrecio(), getTipo());
+    }
 }
